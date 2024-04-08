4fe66fa669dba23adbe37adc880ca9273362ccab
EC2 Default User <ec2-user@ip-172-31-30-84.ec2.internal>
2024-04-08 12:11:08 +0000

    push

--- q1.py
+++ q1.py
@@ -1,7 +1,8 @@
 import sys
 from pyspark.sql import SparkSession
 # you may add more import if you need to
-
+from pyspark.sql.types import StructType, StructField, StringType, FloatType
+from pyspark.sql.functions import col
 
 # don't change this line
 hdfs_nn = sys.argv[1]
@@ -9,4 +10,29 @@ hdfs_nn = sys.argv[1]
 spark = SparkSession.builder.appName("Assigment 2 Question 1").getOrCreate()
 
 # YOUR CODE GOES BELOW
+schema = StructType([
+    StructField("_c0", StringType(), True),
+    StructField("Name", StringType(), True),
+    StructField("City", StringType(), True),
+    StructField("Cuisine Style", StringType(), True),
+    StructField("Ranking", StringType(), True),  # Consider IntegerType if appropriate
+    StructField("Rating", FloatType(), True),   # Consider FloatType or DoubleType if appropriate
+    StructField("Price Range", StringType(), True),
+    StructField("Number of Reviews", StringType(), True),  # Consider IntegerType if appropriate
+    StructField("Reviews", StringType(), True),
+    StructField("URL_TA", StringType(), True),
+    StructField("ID_TA", StringType(), True)
+])
+df = spark.read.option("header", True).schema(schema).csv(f"hdfs://{hdfs_nn}:9000/assignment2/part1/input/TA_restaurants_curated_cleaned.csv")
+# collect = df.collect()
+# print(collect[:5])
+filtered_df = df.filter((col("Reviews") != "") & 
+    (col("Reviews").isNotNull()) & 
+    (col("Rating") >= 1.0))
+# collect = filtered_df.collect()
+# print(collect[:5])
+output_path = f"hdfs://{hdfs_nn}:9000/assignment2/output/question1/"
+filtered_df.write.option("header", "true").csv(output_path)
+
+
 
--- q2.py
+++ q2.py
@@ -2,10 +2,45 @@ import sys
 from pyspark.sql import SparkSession
 
 # you may add more import if you need to
-
+from pyspark.sql.types import StructType, StructField, StringType, FloatType
+from pyspark.sql.functions import col, max, min
 
 # don't change this line
 hdfs_nn = sys.argv[1]
 
 spark = SparkSession.builder.appName("Assigment 2 Question 2").getOrCreate()
 # YOUR CODE GOES BELOW
+schema = StructType([
+    StructField("_c0", StringType(), True),
+    StructField("Name", StringType(), True),
+    StructField("City", StringType(), True),
+    StructField("Cuisine Style", StringType(), True),
+    StructField("Ranking", StringType(), True),  # Consider IntegerType if appropriate
+    StructField("Rating", FloatType(), True),   # Consider FloatType or DoubleType if appropriate
+    StructField("Price Range", StringType(), True),
+    StructField("Number of Reviews", StringType(), True),  # Consider IntegerType if appropriate
+    StructField("Reviews", StringType(), True),
+    StructField("URL_TA", StringType(), True),
+    StructField("ID_TA", StringType(), True)
+])
+df = spark.read.option("header", True).schema(schema).csv(f"hdfs://{hdfs_nn}:9000/assignment2/part1/input/TA_restaurants_curated_cleaned.csv")
+filtered_df = df.filter(
+        (col("Price Range") != "") & 
+        (col("Price Range").isNotNull())
+    ).groupBy(
+        col("Price Range"), col("City")
+    ).agg(
+        max("Rating").alias("Best Rating"),
+        min("Rating").alias("Worst Rating")
+    )
+
+df_max_min_rating = df.join(
+    filtered_df,
+    (df.City == filtered_df.City) & (df["Price Range"] == filtered_df["Price Range"]) & ((df.Rating == filtered_df["Best Rating"] | df.Rating == filtered_df["Worst Rating"])),
+    "inner"
+)
+
+collect = df_max_min_rating.collect()
+print(collect[:6])
+output_path = f"hdfs://{hdfs_nn}:9000/assignment2/output/question2/"
+df_max_min_rating.write.mode("overwrite").option("header", "true").csv(output_path)
